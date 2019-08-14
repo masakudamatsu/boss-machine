e1946534fbbdbc99479865b44c68b30122549b99
Unknown <m.kudamatsu@gmail.com>
2019-08-14 16:40:42 +0900

    DRY the code for finding a minion #5
    This will be used by both GET and PUT, to deal with an invalid ID.

--- server/minions.js
+++ server/minions.js
@@ -3,15 +3,28 @@ const minionsRouter = express.Router();
 
 const db = require('./db.js');
 
+// Check if the requested minion exists
+minionsRouter.use('/:minionId', (req, res, next) => {
+  const foundMinion = db.getFromDatabaseById('minions', req.params.minionId);
+  if (!foundMinion) {
+    res.status(404).send();
+  } else {
+    req.foundMinion = foundMinion;
+    next();
+  }
+});
+
+// GET all
 minionsRouter.get('/', (req, res, next) => {
   const minions = db.getAllFromDatabase('minions');
   res.send(minions);
 });
 
+// GET a specific minion
 minionsRouter.get('/:minionId', (req, res, next) => {
-  const foundMinion = db.getFromDatabaseById('minions', req.params.minionId);
-  if (foundMinion) {
-    res.send(foundMinion);
+  res.send(req.foundMinion);
+});
+
   } else {
     res.status(404).send();
   }

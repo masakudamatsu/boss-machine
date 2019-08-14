decaad6f874ffa12f50399c540b73b382c9f4cb6
Unknown <m.kudamatsu@gmail.com>
2019-08-14 14:47:50 +0900

    Start the server #4

--- server.js
+++ server.js
@@ -23,5 +23,8 @@ app.use('/api', apiRouter);
 // This conditional is here for testing purposes:
 if (!module.parent) {
   // Add your code to start the server listening at PORT below:
+  app.listen(PORT, () => {
+    console.log(`CORS-enabled web server is listening on port ${PORT}`);
+  });
 
 }

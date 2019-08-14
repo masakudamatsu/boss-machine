c004688e05f527bd105bb408f4516bbcaa185d3b
Unknown <m.kudamatsu@gmail.com>
2019-08-14 16:19:28 +0900

    Handle the GET /minions/:minionId request #5

--- server/minions.js
+++ server/minions.js
@@ -8,4 +8,13 @@ minionsRouter.get('/', (req, res, next) => {
   res.send(minions);
 });
 
+minionsRouter.get('/:minionId', (req, res, next) => {
+  const foundMinion = db.getFromDatabaseById('minions', req.params.minionId);
+  if (foundMinion) {
+    res.send(foundMinion);
+  } else {
+    res.status(404).send();
+  }
+});
+
 module.exports = minionsRouter;

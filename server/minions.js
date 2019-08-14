d2a8c937a6a0b590939de63f86a18a2629f6896b
Unknown <m.kudamatsu@gmail.com>
2019-08-14 15:37:26 +0900

    Handle the GET /minions request #5

--- server/minions.js
+++ server/minions.js
@@ -3,4 +3,9 @@ const minionsRouter = express.Router();
 
 const db = require('./db.js');
 
+minionsRouter.get('/', (req, res, next) => {
+  const minions = db.getAllFromDatabase('minions');
+  res.send(minions);
+});
+
 module.exports = minionsRouter;
